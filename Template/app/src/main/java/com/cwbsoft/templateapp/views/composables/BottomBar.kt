5b5c0d47c78fac6bb5463f07040597f16ef9a101
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-10-17 07:42:02 -0300

    Added bottom bar

---
+++ Template/app/src/main/java/com/cwbsoft/templateapp/views/composables/BottomBar.kt
@@ -0,0 +1,82 @@
+package com.cwbsoft.templateapp.views.composables
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BottomAppBarExample() {
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(Icons.Filled.Check, contentDescription = "Localized description")
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Edit,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.CheckCircle,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.AccountBox,
+                            contentDescription = "Localized description",
+                        )
+                    }
+                }
+            )
+        },
+    ) { innerPadding ->
+        Text(
+            modifier = Modifier.padding(innerPadding),
+            text = "Example of a scaffold with a bottom app bar."
+        )
+    }
+}
+
+@Composable
+fun ThreeElementsBottomAppBar() {
+    BottomAppBar(
+        actions = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(Icons.Filled.Check, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    Icons.Filled.Edit,
+                    contentDescription = "Localized description",
+                )
+            }
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    Icons.Filled.CheckCircle,
+                    contentDescription = "Localized description",
+                )
+            }
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    Icons.Filled.AccountBox,
+                    contentDescription = "Localized description",
+                )
+            }
+        }
+    )
+}
--- Template/app/src/main/java/com/cwbsoft/templateapp/views/screens/DummyScreen.kt
+++ Template/app/src/main/java/com/cwbsoft/templateapp/views/screens/DummyScreen.kt
@@ -4,36 +4,49 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cwbsoft.templateapp.R
+import com.cwbsoft.templateapp.views.composables.BottomAppBarExample
 import com.cwbsoft.templateapp.views.composables.CardButton
 import com.cwbsoft.templateapp.views.composables.ElevatedCardButton
 import com.cwbsoft.templateapp.views.composables.FrameImageButton
+import com.cwbsoft.templateapp.views.composables.ThreeElementsBottomAppBar
 
 
 @Composable
 fun DummyScreen(modifier: Modifier)
 {
-    Column (modifier = Modifier.padding(24.dp)){
-        CardButton(
-            title = stringResource(id = R.string.dummy_title),
-            content = stringResource(id = R.string.dummy_content))
-        
-        Spacer(modifier = Modifier.height(12.dp))
+    Scaffold(
+        bottomBar = { ThreeElementsBottomAppBar() }
+    )
+    { innerPadding ->
+        Column (modifier = Modifier.padding(innerPadding)){
+            CardButton(
+                title = stringResource(id = R.string.dummy_title),
+                content = stringResource(id = R.string.dummy_content))
 
-        ElevatedCardButton(
-            content = stringResource(id = R.string.dummy_content))
+            Spacer(modifier = Modifier.height(12.dp))
 
-        Spacer(modifier = Modifier.height(12.dp))
+            ElevatedCardButton(
+                content = stringResource(id = R.string.dummy_content))
 
-        FrameImageButton(
-            title = "FrameImageButton",
-            content = stringResource(id = R.string.dummy_content))
+            Spacer(modifier = Modifier.height(12.dp))
+
+            FrameImageButton(
+                title = "FrameImageButton",
+                content = stringResource(id = R.string.dummy_content))
+
+        }
     }
+
+
+
 
 }
 
