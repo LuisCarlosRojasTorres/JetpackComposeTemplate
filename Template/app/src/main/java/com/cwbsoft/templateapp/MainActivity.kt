92e5c328573835a9e90e4b8c490b5438c0ce8d1c
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-10-30 20:20:42 -0300

    Added scafold

--- Template/app/src/main/java/com/cwbsoft/templateapp/MainActivity.kt
+++ Template/app/src/main/java/com/cwbsoft/templateapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,16 +21,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TemplateAppTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MyApp()
+                Scaffold (){
+                        innerPadding ->
+
+                    MyApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun MyApp(){
-    DummyScreen(Modifier.padding(24.dp))
+fun MyApp(modifier: Modifier){
+    DummyScreen(modifier)
 }
