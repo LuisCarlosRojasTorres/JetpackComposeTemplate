765fb593ef693986a3f610cc7712971cb25f96d1
Felipe Benghi <fbenghi@gmail.com>
2024-10-30 21:52:44 -0300

    Add group of sliders with customizable icon and steps

--- Template/app/src/main/java/com/cwbsoft/templateapp/views/screens/DummyScreen.kt
+++ Template/app/src/main/java/com/cwbsoft/templateapp/views/screens/DummyScreen.kt
@@ -4,19 +4,34 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cwbsoft.templateapp.R
-import com.cwbsoft.templateapp.views.composables.BottomAppBarExample
 import com.cwbsoft.templateapp.views.composables.CardButton
 import com.cwbsoft.templateapp.views.composables.ElevatedCardButton
 import com.cwbsoft.templateapp.views.composables.FrameImageButton
 import com.cwbsoft.templateapp.views.composables.ThreeElementsBottomAppBar
+import com.cwbsoft.templateapp.views.composables.sliderwithcustomthumb.Categories
+import com.cwbsoft.templateapp.views.composables.sliderwithcustomthumb.CategoriesUI
+import com.cwbsoft.templateapp.views.composables.sliderwithcustomthumb.Category
+import com.cwbsoft.templateapp.views.composables.sliderwithcustomthumb.CategoryIcon
+import com.cwbsoft.templateapp.views.composables.sliderwithcustomthumb.CategoryUI
+import com.cwbsoft.templateapp.views.composables.sliderwithcustomthumb.GeneralCategoriesParameters
+import com.cwbsoft.templateapp.views.composables.sliderwithcustomthumb.SliderWithCustomThumb
 
 
 @Composable
@@ -26,7 +41,9 @@ fun DummyScreen(modifier: Modifier)
         bottomBar = { ThreeElementsBottomAppBar() }
     )
     { innerPadding ->
-        Column (modifier = Modifier.padding(innerPadding)){
+        Column (
+            modifier = Modifier.padding(innerPadding).verticalScroll(rememberScrollState())
+        ){
             CardButton(
                 title = stringResource(id = R.string.dummy_title),
                 content = stringResource(id = R.string.dummy_content))
@@ -42,6 +59,20 @@ fun DummyScreen(modifier: Modifier)
                 title = "FrameImageButton",
                 content = stringResource(id = R.string.dummy_content))
 
+            val categoriesPoints = fetchCategoriesPoints()
+            val categoriesUi = initializeCategoriesUis()
+
+
+            SliderWithCustomThumb(
+                categories = categoriesPoints,
+                categoriesUI = categoriesUi,
+                generalParameters = GeneralCategoriesParameters(
+                    numberOfSteps = 4,
+                    range = 0f..5f,
+                ),
+                modifier = Modifier.padding(innerPadding)
+            )
+
         }
     }
 
@@ -55,4 +86,76 @@ fun DummyScreen(modifier: Modifier)
 fun DummyScreenPreview()
 {
     DummyScreen(Modifier.padding(24.dp))
+}
+
+fun fetchCategoriesPoints(): Categories{
+    return Categories(listOf(
+        Category(
+            categoryId = "family",
+            value = 1f,
+        ),
+        Category(
+            categoryId = "money",
+            value = 2f,
+        ),
+        Category(
+            categoryId = "love",
+            value = 3f,
+        ),
+        Category(
+            categoryId = "health",
+            value = 3f,
+        )
+        ,
+        Category(
+            categoryId = "work",
+            value = 3f,
+        )
+    ))
+}
+
+@Composable
+fun initializeCategoriesUis(): CategoriesUI{
+    return CategoriesUI(listOf(
+        CategoryUI(
+            name = stringResource(id = R.string.family),
+            categoryId = "family",
+            thumbColor = Color.DarkGray,
+            activeTrackColor = MaterialTheme.colorScheme.secondary,
+            inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            icon = CategoryIcon(imageVector = Icons.Filled.AccountCircle, contentDescription = null)
+        ),
+        CategoryUI(
+            name = stringResource(id = R.string.money),
+            categoryId = "money",
+            thumbColor = Color.Yellow,
+            activeTrackColor = MaterialTheme.colorScheme.secondary,
+            inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            icon = CategoryIcon(imageVector = Icons.Filled.Home, contentDescription = null)
+        ),
+        CategoryUI(
+            name = stringResource(id = R.string.love),
+            categoryId = "love",
+            thumbColor = Color.Red,
+            activeTrackColor = MaterialTheme.colorScheme.secondary,
+            inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            icon = CategoryIcon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+        ),
+        CategoryUI(
+            name = stringResource(id = R.string.health),
+            categoryId = "health",
+            thumbColor = Color.Green,
+            activeTrackColor = MaterialTheme.colorScheme.secondary,
+            inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            icon = CategoryIcon(imageVector = Icons.Filled.AddCircle, contentDescription = null)
+        ),
+        CategoryUI(
+            name = stringResource(id = R.string.work),
+            categoryId = "work",
+            thumbColor = Color.Green,
+            activeTrackColor = MaterialTheme.colorScheme.secondary,
+            inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            icon = CategoryIcon(imageVector = Icons.Filled.Edit, contentDescription = null)
+        )
+    ))
 }
